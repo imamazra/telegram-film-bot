dcf1688c4ee95528865c221f704907e8c356032c
imamazra <imamazra6@gmail.com>
2026-06-17 23:55:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_USERNAME = "@FilmViralIndob"
 
 SHOPEE_LINKS = [
-    "https://s.shopee.co.id/LkwJwTI9Y",
+    "https://s.shopee.co.id/8fQ4FdnsBK",
     "https://s.shopee.co.id/gNm2SsiT1",
     "https://s.shopee.co.id/5L9bb6ZKoo"
 ]
