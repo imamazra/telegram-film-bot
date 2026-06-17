4efdf90d7187a53b05ad6b632279b150524df5bb
imamazra <imamazra6@gmail.com>
2026-06-17 13:36:32 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+CHANNEL_USERNAME = "@FilmViralIndob"
+
+SHOPEE_LINKS = [
+    "https://s.shopee.co.id/50WlCNqdAU",
+    "https://s.shopee.co.id/gNm2SsiT1",
+    "https://s.shopee.co.id/5L9bb6ZKoo"
+]
+
+FILM_VIDEOS = [
+    "https://t.me/video1",
+    "https://t.me/video2"
+]
